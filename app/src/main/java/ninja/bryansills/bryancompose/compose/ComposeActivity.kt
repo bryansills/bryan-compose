7bf74dff07b0dc22009cea5604ec1651be7bcba0
Bryan Sills <bsills@bignerdranch.com>
2020-04-14 15:08:55 -0500

    App compiles but the non-button text isn't visible

--- app/src/main/java/ninja/bryansills/bryancompose/compose/ComposeActivity.kt
+++ app/src/main/java/ninja/bryansills/bryancompose/compose/ComposeActivity.kt
@@ -2,22 +2,13 @@ package ninja.bryansills.bryancompose.compose
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.compose.Composable
-import androidx.compose.composer // You need to import this manually
-import androidx.compose.setContent
 import androidx.compose.state
-import androidx.compose.unaryPlus
-import androidx.ui.core.CraneWrapper
-import androidx.ui.core.Text
-import androidx.ui.core.dp
-import androidx.ui.core.sp
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
 import androidx.ui.layout.Column
-import androidx.ui.layout.HeightSpacer
-import androidx.ui.layout.Padding
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.themeTextStyle
 
 class ComposeActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,36 +19,29 @@ class ComposeActivity : Activity() {
 
 @Composable
 fun BryanApp() {
-    CraneWrapper {
         MaterialTheme {
             BryanBody()
         }
-    }
 }
 
 @Composable
 fun BryanBody() {
-    val count = +state { 0 }
-    Padding(padding = 16.dp) {
+    var count by state { 0 }
         Column {
             Text(
                 text = "This is my first test",
-                style = +themeTextStyle { body1 }
+                style = MaterialTheme.typography.body1
             )
-            HeightSpacer(height = 10.dp)
             Text(
                 text = "This is more text after a spacer",
-                style = +themeTextStyle { body2 }
+                style = MaterialTheme.typography.body2
             )
-            HeightSpacer(height = 10.dp)
             Text(
-                text = "This is the current count: ${count.value}",
-                style = +themeTextStyle { body2 }
+                text = "This is the current count: $count",
+                style = MaterialTheme.typography.body2
             )
             Button(
-                text = "THIS IS A BUTTON",
-                onClick = { count.value += 1 }
-            )
+                onClick = { count += 1 }
+            ) { Text(text = "THIS IS A BUTTON WITH COUNT: $count") }
         }
-    }
 }
